71e871c2ddda45bf0697fd710c58285f85138149
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-08 19:40:46 +0100

    dev: Added docs

--- core/src/main/java/dev/dotspace/common/concurrent/v2/ResponseConsumer.java
+++ core/src/main/java/dev/dotspace/common/concurrent/v2/ResponseConsumer.java
@@ -1,13 +1,26 @@
 package dev.dotspace.common.concurrent.v2;
 
-import dev.dotspace.common.concurrent.v2.implementation.State;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * This consumer passes the information of a {@link ResponseConsumer}.
+ * A normal {@link java.util.function.Consumer} was not usable, because this one now passes a value.
+ *
+ * @param <TYPE> defines the type of {@link Response} type.
+ */
 public interface ResponseConsumer<TYPE> {
-
+  /**
+   * This method is filled in by the {@link Response}.
+   * Typically, this method is used when completing the answer.
+   *
+   * @param state     the current {@link State} of the response.
+   * @param type      keeps the value of the answer if any. Otherwise, null.
+   *                  (Not null if state equals {@link State#COMPLETED_DEFAULT})
+   * @param throwable keeps the completed error message if available. Otherwise, null.
+   *                  (Can only be present if state equals {@link State#COMPLETED_EXCEPTIONALLY})
+   */
   void accept(@NotNull final State state,
               @Nullable final TYPE type,
               @Nullable final Throwable throwable);
-
 }
