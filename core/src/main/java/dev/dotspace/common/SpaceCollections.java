a975baac5907225bbb4e721ce2bdabd597b5cc2a
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-14 11:22:29 +0100

    dev: Removed array implementations -> moved to SpaceArrays.java

--- core/src/main/java/dev/dotspace/common/SpaceCollections.java
+++ core/src/main/java/dev/dotspace/common/SpaceCollections.java
@@ -9,9 +9,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
+ * Methods for simplifying collections and arrays.
+ * <p>
  * Class with {@link Collection} operations.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE) //Block class construction.
@@ -26,24 +27,10 @@ public final class SpaceCollections {
    */
   @SuppressWarnings("unchecked")
   public static <ELEMENT> @Nullable ELEMENT random(@Nullable final Collection<ELEMENT> collection) {
-    if (collection == null || collection.isEmpty()) {
+    if (SpaceObjects.throwIfNull(collection).isEmpty()) {
       return null; //Return null to safe performance.
     }
-    return (ELEMENT) SpaceCollections.randomImplementation(collection.toArray());
-  }
-
-  /**
-   * Get a random element of array of any type.
-   *
-   * @param array     to get random element of.
-   * @param <ELEMENT> generic type of element to get random.
-   * @return random drawn element or null if array is null or empty.
-   */
-  public static <ELEMENT> @Nullable ELEMENT random(@Nullable final ELEMENT[] array) {
-    if (array == null || array.length == 0) {
-      return null; //Return null to safe performance.
-    }
-    return SpaceCollections.randomImplementation(array);
+    return (ELEMENT) collection.toArray()[LibraryCommonUtils.calculateRandomIndex(collection.size())];
   }
 
   /**
@@ -57,21 +44,5 @@ public final class SpaceCollections {
    */
   public static <ELEMENT> @NotNull FutureResponse<ELEMENT> randomAsync(@Nullable final Collection<ELEMENT> collection) {
     return new FutureResponse<ELEMENT>().completeAsync(() -> SpaceCollections.random(collection)); //Complete the future in a separate thread
-  }
-
-  public static <ELEMENT> @NotNull FutureResponse<ELEMENT> randomAsync(@Nullable final ELEMENT[] array) {
-    return new FutureResponse<ELEMENT>().completeAsync(() -> SpaceCollections.random(array)); //Complete the future in a separate thread
-  }
-
-  /**
-   * Implementation to get random object of array. Position is calculated by using a {@link java.util.Random}.
-   *
-   * @param array     to get random element from.
-   * @param <ELEMENT> generic type of elements of array.
-   * @return random element, null if given array has less than 1 member.
-   */
-  private static <ELEMENT> @Nullable ELEMENT randomImplementation(@Nullable final ELEMENT[] array) {
-    final int index = array.length > 1 ? (int) (ThreadLocalRandom.current().nextDouble() * array.length) : 0; //Calculate random index to get from collection.
-    return array[index]; //Return random object of list.
   }
 }
