27af12e984efc71a6135558b190d439f4c31cfc8
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-15 22:10:51 +0100

    dev: Added methods to create reponse.

--- core/src/main/java/dev/dotspace/common/SpaceLibrary.java
+++ core/src/main/java/dev/dotspace/common/SpaceLibrary.java
@@ -1,8 +1,50 @@
 package dev.dotspace.common;
 
+import dev.dotspace.common.concurrent.CompletableResponse;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SpaceLibrary {
+  /**
+   * Create a new {@link CompletableResponse} instance.
+   *
+   * @param <TYPE> type to use for response.
+   * @return new instance of {@link CompletableResponse}.
+   */
+  public static <TYPE> @NotNull CompletableResponse<TYPE> response() {
+    return new CompletableResponse<>();
+  }
+
+  /**
+   * @param typeClass
+   * @param <TYPE>
+   * @return
+   */
+  public static <TYPE> @NotNull CompletableResponse<TYPE> response(@Nullable final Class<TYPE> typeClass) {
+    return new CompletableResponse<>(typeClass);
+  }
+
+  /**
+   * @param type
+   * @param <TYPE>
+   * @return
+   */
+  public static <TYPE> @NotNull CompletableResponse<TYPE> completeResponse(@Nullable TYPE type) {
+    return new CompletableResponse<TYPE>().complete(type);
+  }
+
+  /**
+   * @param typeSupplier
+   * @param <TYPE>
+   * @return
+   * @throws NullPointerException if typeSupplier is null.
+   */
+  public static <TYPE> @NotNull CompletableResponse<TYPE> completeResponseAsync(@Nullable Supplier<TYPE> typeSupplier) {
+    return new CompletableResponse<TYPE>().completeAsync(typeSupplier);
+  }
 }
