e7a6a71e512bb446d27ac87dcd455563b087e451
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-15 20:39:14 +0100

    dev: Added docs

--- core/src/main/java/dev/dotspace/common/LibraryCommonUtils.java
+++ core/src/main/java/dev/dotspace/common/LibraryCommonUtils.java
@@ -2,18 +2,22 @@ package dev.dotspace.common;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.Range;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Methods across classes for the library.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 final class LibraryCommonUtils {
   /**
    * Get random index.
    *
-   * @param arrayLength
-   * @return
+   * @param arrayLength to get array from of. [between 0 and {@link Integer#MAX_VALUE}]
+   * @return random calculated position(index)
    */
-  static int calculateRandomIndex(final int arrayLength) {
+  static int calculateRandomIndex(@Range(from = 0, to = Integer.MAX_VALUE) final int arrayLength) {
     return arrayLength > 1 ? (int) (ThreadLocalRandom.current().nextDouble() * arrayLength) : 0;  //Calculate random index to get from collection.
   }
 }
