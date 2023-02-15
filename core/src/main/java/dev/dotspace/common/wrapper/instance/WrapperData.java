4b32d65ffbb3d5098e64c540ada78f55b81fa372
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-15 20:39:41 +0100

    dev: Added docs

--- core/src/main/java/dev/dotspace/common/wrapper/instance/WrapperData.java
+++ core/src/main/java/dev/dotspace/common/wrapper/instance/WrapperData.java
@@ -6,11 +6,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.UUID;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface WrapperData {
-
+  /**
+   * Name of the specific wrapper.
+   *
+   * @return name of the wrapper.
+   */
   @NotNull String name();
 
   /**
