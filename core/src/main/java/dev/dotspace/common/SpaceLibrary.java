acef9f021b1c3e0867283b10fea00e69c1d773e3
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-15 20:39:31 +0100

    dev: Added SpaceLibrary as main class for library

---
+++ core/src/main/java/dev/dotspace/common/SpaceLibrary.java
@@ -0,0 +1,8 @@
+package dev.dotspace.common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SpaceLibrary {
+}
