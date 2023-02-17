cc220e3564a7090e358f35a2b80008637601c211
SubwayKeks <KArina.Klintz@gmx.de>
2023-02-17 22:34:34 +0100

    dev: Added annotation and another method

--- core/src/main/java/dev/dotspace/common/concurrent/ResponseFunction.java
+++ core/src/main/java/dev/dotspace/common/concurrent/ResponseFunction.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 /**
  * @param <TYPE>
  */
+@FunctionalInterface
 public interface ResponseFunction<TYPE> {
   /**
    * Executes the function of the class.
@@ -14,4 +15,11 @@ public interface ResponseFunction<TYPE> {
    * @param executorService when a thread service is needed.
    */
   void run(@Nullable final ExecutorService executorService);
+
+  /**
+   * Execute without service, only synchronized operations are possible.
+   */
+  default void run() {
+    this.run(null /*No service for run process.*/);
+  }
 }
