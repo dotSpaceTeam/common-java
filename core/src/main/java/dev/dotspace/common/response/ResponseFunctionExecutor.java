cf90504b81f9795830344245b7fb191e1b2cba12
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-24 11:48:16 +0100

    dev: Added alternate constructor with no condition check in ResponseFunctionExecutor.java

--- core/src/main/java/dev/dotspace/common/response/ResponseFunctionExecutor.java
+++ core/src/main/java/dev/dotspace/common/response/ResponseFunctionExecutor.java
@@ -11,18 +11,18 @@ import java.util.function.Supplier;
  * @param <TYPE>
  */
 final class ResponseFunctionExecutor<TYPE> implements ResponseFunction<TYPE> {
-  private final @NotNull Supplier<Boolean> checkIfExecutable;
+  private final @Nullable Supplier<Boolean> checkIfExecutable;
   private final @NotNull Runnable runnable;
   private final boolean async;
 
   /**
    * Creates instance with all needed values.
    *
-   * @param checkIfExecutable
-   * @param runnable
-   * @param async
+   * @param checkIfExecutable condition to check on execute.
+   * @param runnable to run.
+   * @param async true, if {@link Runnable} should be executed asynchronous.
    */
-  ResponseFunctionExecutor(@NotNull final Supplier<Boolean> checkIfExecutable,
+  ResponseFunctionExecutor(@Nullable final Supplier<Boolean> checkIfExecutable,
                            @NotNull Runnable runnable,
                            final boolean async) {
     this.checkIfExecutable = SpaceObjects.throwIfNull(checkIfExecutable);
@@ -30,9 +30,20 @@ final class ResponseFunctionExecutor<TYPE> implements ResponseFunction<TYPE> {
     this.async = async;
   }
 
+  /**
+   * Create instance with no checkIfExecutable.
+   *
+   * @param runnable to run.
+   * @param async true, if {@link Runnable} should be executed asynchronous.
+   */
+  ResponseFunctionExecutor(@NotNull Runnable runnable,
+                           final boolean async) {
+    this(null, runnable, async);
+  }
+
   @Override
   public void run(@Nullable final ExecutorService executorService) {
-    if (!this.checkIfExecutable.get()) {
+    if (this.checkIfExecutable != null && !this.checkIfExecutable.get()) {
       return;
     }
     if (this.async && executorService != null) {
