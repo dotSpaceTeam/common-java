094f310ba1dd135a3e7efd5d5b4c6a39871859a1
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-24 12:40:16 +0100

    dev: Added docs for class ResponseFunctionExecutor.java

--- core/src/main/java/dev/dotspace/common/response/ResponseFunctionExecutor.java
+++ core/src/main/java/dev/dotspace/common/response/ResponseFunctionExecutor.java
@@ -8,7 +8,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.function.Supplier;
 
 /**
- * @param <TYPE>
+ * Implementation of {@link ResponseFunction}, used for {@link CompletableResponse}.
+ *
+ * @see ResponseFunction
  */
 final class ResponseFunctionExecutor<TYPE> implements ResponseFunction<TYPE> {
   private final @Nullable Supplier<Boolean> checkIfExecutable;
@@ -19,8 +21,8 @@ final class ResponseFunctionExecutor<TYPE> implements ResponseFunction<TYPE> {
    * Creates instance with all needed values.
    *
    * @param checkIfExecutable condition to check on execute.
-   * @param runnable to run.
-   * @param async true, if {@link Runnable} should be executed asynchronous.
+   * @param runnable          to run.
+   * @param async             true, if {@link Runnable} should be executed asynchronous.
    */
   ResponseFunctionExecutor(@Nullable final Supplier<Boolean> checkIfExecutable,
                            @NotNull Runnable runnable,
@@ -34,22 +36,25 @@ final class ResponseFunctionExecutor<TYPE> implements ResponseFunction<TYPE> {
    * Create instance with no checkIfExecutable.
    *
    * @param runnable to run.
-   * @param async true, if {@link Runnable} should be executed asynchronous.
+   * @param async    true, if {@link Runnable} should be executed asynchronous.
    */
   ResponseFunctionExecutor(@NotNull Runnable runnable,
                            final boolean async) {
     this(null, runnable, async);
   }
 
+  /**
+   * @see ResponseFunction#run(ExecutorService)
+   */
   @Override
   public void run(@Nullable final ExecutorService executorService) {
-    if (this.checkIfExecutable != null && !this.checkIfExecutable.get()) {
+    if (this.checkIfExecutable != null && !this.checkIfExecutable.get()) { //Do not execute if checkIfExecutable is present an result is true.
       return;
     }
-    if (this.async && executorService != null) {
+    if (this.async && executorService != null) { //If async is true and executorService is present, run with executorService.
       executorService.execute(this.runnable);
     } else {
-      this.runnable.run();
+      this.runnable.run(); //Run default.
     }
   }
 }
