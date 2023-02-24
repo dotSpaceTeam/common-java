cd1fd3c5762d124e8de5f5427bc5b1ca2d49adf5
Day-Hawk <daniel.riethmueller@gmx.net>
2023-02-24 17:45:02 +0100

    dev: Added doc

--- base/src/main/java/dev/dotspace/annotation/AvailableTests.java
+++ base/src/main/java/dev/dotspace/annotation/AvailableTests.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * To keep track of tests. Each method that has a test receives the annotation.
+ */
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface AvailableTests {
