38c34e5ab36449536b2ed843dee1f068e1690788
Day-Hawk <daniel.riethmueller@gmx.net>
2023-03-27 08:37:22 +0200

    dev: Added packages to module-info.

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,5 +1,25 @@
 module dev.dotspace.common {
   requires org.jetbrains.annotations;
   requires lombok;
+
+  //Base Library
   exports dev.dotspace.common;
+
+  //Annotations
+  exports dev.dotspace.common.annotation;
+
+  //Java concurrent bridge
+  exports dev.dotspace.common.concurrent;
+
+  //Exceptions
+  exports dev.dotspace.common.exception;
+
+  //Lists: Pagination
+  exports dev.dotspace.common.list;
+
+  //Math
+  exports dev.dotspace.common.math;
+
+  //Response package
+  exports dev.dotspace.common.response;
 }
